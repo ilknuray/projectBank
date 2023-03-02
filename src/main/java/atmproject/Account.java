5be459c8ac177b058c31f51998f8e3592d36857e
ilknur yilmaz <ilknuray1318@gmail.com>
2023-03-02 21:27:15 +0100

    First

---
+++ src/main/java/atmproject/Account.java
@@ -0,0 +1,9 @@
+package atmproject;
+
+public class Account {
+    private int accountNumber;
+    private int pinNumber;
+
+    private double savingBalance;
+    private double checkingBalance;
+}
