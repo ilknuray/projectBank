d3ec0e2b54cab6b84bfa81b6cbd498e37f0a219f
ilknur yilmaz <ilknuray1318@gmail.com>
2023-03-02 21:24:37 +0100

    First

--- src/main/java/atmproject/hazirlik.java
+++ src/main/java/atmproject/hazirlik.java
@@ -5,5 +5,18 @@ public class hazirlik {
     Pseudo code : (sudo kod)=>
     Java da siniflama ve logigi olusturma cok onemlidir.
 
+    checking account=vadesiz hesap
+    saving account=vadeli hesap
+
+    para cekme=withdraw
+    para yatirma=deposit
+
+    double checking balance; vadeli icin bakiye
+    double savingBalance; vadesiz icin bakiye
+    (para kullaniiyorsa double yapilmasi mantklidit)
+
+    int accauntNumber=hesap numarasi
+     int pinNumber=sifre
+
      */
 }
