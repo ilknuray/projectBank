0c579cb1eff224bd4d50ca116cb6a8e311005089
ilknur yilmaz <ilknuray1318@gmail.com>
2023-03-04 11:28:03 +0100

    First

--- src/main/java/atmproject/AtmRunner.java
+++ src/main/java/atmproject/AtmRunner.java
@@ -1,4 +1,8 @@
 package atmproject;
 
 public class AtmRunner {
+    public static void main(String[] args) {
+        Account account=new Account();
+        account.getCheckingWithDraw();
+    }
 }
